d8b0953319f5c6fa1e2cf4d1135d8be4630acd77
elian.marchisio@gmail.com <elian.marchisio@gmail.com@80bb64d5-4173-62ce-5e7d-32d8d86d7bbf>
2012-04-27 14:40:35 +0000

    correcciones a Subject y subjectManagementController, falta modificar las conexiones a la BD para buscar y borrar materia

--- src/educar/controllers/AdminController/subjectManagementController.java
+++ src/educar/controllers/AdminController/subjectManagementController.java
@@ -3,15 +3,14 @@ package educar.controllers.AdminController;
 import educar.controllers.IController;
 import educar.gui.IView;
 import educar.gui.AdminViews.SubjectView;
+import educar.models.Alumno;
 import educar.models.Subject;
 
 public class subjectManagementController implements IController {
 
 	private SubjectView view;
 	private Subject materia;
-	
-	
-	
+
 	@Override
 	public void setView(IView view) {
 		// TODO Auto-generated method stub
@@ -25,18 +24,41 @@ public class subjectManagementController implements IController {
 			view.setEmptyFields();
 		}
 		if (model.compareTo("Buscar") == 0) {
-			materia = Subject.getSubject();
-			view.setModifyCodeTF(materia.getSubject());
-			view.setModifyNameTF(materia.getName());
+			materia = materia.getSubject();
+			if (materia != null) {
+				actualizarView();
+			} else {
+				view.present("La materia no existe");
+			}
 		}
 		if (model.compareTo("Delete") == 0) {
-			materia.deleteSubject();
+			if (materia.deleteSubject())
+				view.present("Materia borrada exitosamente");
+			else
+				view.present("La materia no existe");
 		}
-		
-		// hacer que borre todos los espacios en blanco!! 
-		
-	}
-	private 
-	
 
+		// hacer que borre todos los espacios en blanco!!
+
+	}
+
+	private void actualizarView() {
+		view.setModifyCodeTF(materia.getCode());
+		view.setModifyNameTF(materia.getName());
+	}
+
+	// modificar para realizar la conexion a la base de datos
+	private void getSubjectFromDB() {
+		// TODO Auto-generated method stub
+		materia.setCode(view.getModifyCodeTF());
+		materia = materia.getSubject();
+		if (materia != null) {
+			view.setModifyCodeTF(materia.getCode());
+			view.setModifyNameTF(materia.getName());
+		} else {
+			view.present("El codigo de la materia no existe o es incorrecto");
+			view.setEmptyFields();
+		}
+
+	}
 }
