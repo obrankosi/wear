ded7eb418364112a33cdea4348e4edea0386ffd4
coriasaul@gmail.com <coriasaul@gmail.com@80bb64d5-4173-62ce-5e7d-32d8d86d7bbf>
2012-05-01 23:06:21 +0000

    cambien la interfas de rootview , agregue la funcionalidad de borrar.
    plante la extructura para cargos con los enlaces de botones hecho

---
+++ src/educar/controllers/ChargeController.java
@@ -0,0 +1,25 @@
+package educar.controllers;
+
+import educar.gui.IView;
+import educar.gui.RootView;
+import educar.gui.AdminViews.administrador;
+import educar.languaje.defaultLanguaje;
+import educar.models.User;
+import educar.models.userNotFound;
+
+public class ChargeController implements IController, defaultLanguaje {
+    private static User docente;
+    private administrador rView;
+	
+    @Override
+	public void process(String model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+		@Override
+		public void setView(IView view) {
+			// TODO Auto-generated method stub
+			
+		}
+}
