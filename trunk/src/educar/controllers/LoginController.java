1c2a188a769a23b44486e90c3e97df0c52af6dc0
coriasaul@gmail.com <coriasaul@gmail.com@80bb64d5-4173-62ce-5e7d-32d8d86d7bbf>
2012-05-03 03:55:22 +0000

    terminada la vista para administrador

--- trunk/src/educar/controllers/LoginController.java
+++ trunk/src/educar/controllers/LoginController.java
@@ -1,6 +1,5 @@
 package educar.controllers;
 
-//import educar.gui.AdminView;
 import educar.gui.IView;
 import educar.gui.LoginView;
 import educar.gui.RootView;
@@ -9,42 +8,42 @@ import educar.models.Session;
 import educar.models.User;
 
 public class LoginController implements IController {
-    private LoginView view;
+	private LoginView view;
 
-    public void process(String model) {
-	if (null != view)
-	    if (User.authenticate(((LoginView) view).getUsername(),
-		    ((LoginView) view).getPassword())) {
-		view.present("You are logged in as: "
-			+ Session.getCurrentUser().getUsername());
-		view.close();
-		openView(Session.getCurrentUser());
+	public void process(String model) {
+		if (null != view)
+			if (User.authenticate(((LoginView) view).getUsername(),
+					((LoginView) view).getPassword())) {
+				view.present("You are logged in as: "
+						+ Session.getCurrentUser().getUsername());
+				view.close();
+				openView(Session.getCurrentUser());
 
-	    } else {
-		view.present("Wrong username/password");
-	    }
-    }
-
-    private void openView(User currentUser) {
-	String role = Session.getCurrentUser().getRole();
-	// Segun el tipo de usario, es la View que abre
-	if (role.equals("root")) {
-	    //view.close();
-	    new RootView();
+			} else {
+				view.present("Wrong username/password");
+			}
 	}
-	if (role.equals("admin")) {
-	    new administrador();
-	}
-	// if (role.equals("teacher")) {
-	// goTeacherWindow();
-	// }
-	// if (role.equals("student")) {
-	// goStudentWindow();
-	// }
-    }
 
-    @Override
-    public void setView(IView view) {
-	this.view = (LoginView) view;
-    }
+	private void openView(User currentUser) {
+		String role = Session.getCurrentUser().getRole();
+		// Segun el tipo de usario, es la View que abre
+		if (role.equals("root")) {
+			// view.close();
+			new RootView();
+		}
+		if (role.equals("admin")) {
+			new administrador();
+		}
+		// if (role.equals("teacher")) {
+		// goTeacherWindow();
+		// }
+		// if (role.equals("student")) {
+		// goStudentWindow();
+		// }
+	}
+
+	@Override
+	public void setView(IView view) {
+		this.view = (LoginView) view;
+	}
 }
--- trunk/src/educar/db/DbConnection.java
+++ trunk/src/educar/db/DbConnection.java
@@ -1,13 +1,12 @@
 package educar.db;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
-
-import com.mysql.jdbc.Connection;
 
 public class DbConnection {
 	public static String bd = "educar_dev";
 	private static String login = "root";
-	private static String password = "1";
+	private static String password = "root";
 	private static String url = "jdbc:mysql://localhost/"+bd;
 	private static DbConnection instance;
 	private static Connection conn;
