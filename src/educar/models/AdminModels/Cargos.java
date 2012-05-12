0060a8ed1505bb00fe7804a34bd54d5f28e91e7a
coriasaul@gmail.com <coriasaul@gmail.com@80bb64d5-4173-62ce-5e7d-32d8d86d7bbf>
2012-05-12 00:06:39 +0000

    termine cargo

--- src/educar/models/AdminModels/Cargos.java
+++ src/educar/models/AdminModels/Cargos.java
@@ -1,44 +1,138 @@
 package educar.models.AdminModels;
 
-	
-	import java.sql.PreparedStatement;
-	import java.sql.ResultSet;
-	import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 
 import educar.db.JPA;
 
-	public class Cargos {
-		private static JPA jpa = new JPA();
-		private String codigoCargo;
-		private String nombreCargo;
-	
+public class Cargos {
+	private static JPA jpa = new JPA();
+	private String codigoCargo;
+	private String nombreCargo;
+	private static ResultSet rst;
 
-		/**
-		 * @param codigo cargo
-		 * @param nombre cargo
-		 */
-		public Cargos(String codigoCargo, String nombreCargo) {
-			this.setCodigoCargo(codigoCargo);
-			this.setNombreCargo(nombreCargo);
+	/**
+	 * @param codigo
+	 *            cargo
+	 * @param nombre
+	 *            cargo
+	 */
+	public Cargos(String codigoCargo, String nombreCargo) {
+		this.setCodigoCargo(codigoCargo);
+		this.setNombreCargo(nombreCargo);
 
+	}
+
+	public Cargos(String nombreCargo) {
+		this.setNombreCargo(nombreCargo);
+
+	}
+
+	public static Cargos getCargo(String cod) {
+		try {
+			return getCodigo(cod, "cargo");
+		} catch (CargosNotFound e) {
+			// TODO Auto-generated catch block
+			return null;
 		}
 
+	}
 
-		
-		
-		
-		
-		
-		
-		private void setNombreCargo(String nombreCargo2) {
-			this.nombreCargo=nombreCargo2;
-			
+	private static Cargos getCodigo(String cod, String tableName)
+			throws CargosNotFound {
+		Cargos cargo = null;
+		ResultSet rs = null;
+		rs = jpa.getByField(tableName, "cod_cargo", cod);
+		try {
+			if (rs.next()) {
+				cargo = new Cargos(rs.getString(1), rs.getString(2));
+
+			} else {
+				throw new CargosNotFound();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
+		return cargo;
+	}
 
-		private void setCodigoCargo(String codigoCargo2) {
-			this.codigoCargo=codigoCargo2;
-			
+	public boolean save() {
+		String[] columns = { "cod_cargo", "descripcion_cargo" };
+		try {
+			return save("cargo", columns);
+		} catch (CargosNotFound e) {
+			return false;// TODO Auto-generated catch block
 		}
+	}
 
-}
+	private boolean save(String tableName, String[] columns)
+			throws CargosNotFound {
+		try {
+			String[] column = columns;
+			PreparedStatement stm = jpa.newRecord("educar_dev." + tableName,
+					column);
+
+			 stm.setString(1,codigoCargo);
+			stm.setString(2, nombreCargo);
+			jpa.create(stm);
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+    public static LinkedList<String> listaCargos() throws SQLException {
+	rst = jpa.proyeccion("cargo", "cod_cargo,descripcion_cargo");
+	LinkedList<String> result = new LinkedList<String>();
+	while (rst.next()) {
+	    result.add(rst.getString(1) + " " + rst.getString(2) + " ");
+	}
+	return result;
+    }
+//------------------------------------------------------------------------------------
+	private void setNombreCargo(String nombreCargo2) {
+		this.nombreCargo = nombreCargo2;
+
+	}
+
+	private void setCodigoCargo(String codigoCargo2) {
+		this.codigoCargo = codigoCargo2;
+
+	}
+
+	public String getNombreCargo() {
+		return this.nombreCargo;
+	}
+
+	public String getCodigoCargo() {
+		return this.codigoCargo;
+	}
+
+	public void update(String[] values, String codigoCargo2)
+			throws SQLException {
+		String[] columns = { "descripcion_cargo" };
+		try {
+			jpa.update("cargo", columns, values, "cod_cargo", codigoCargo2);
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new SQLException();
+
+		}
+	}
+
+	public boolean destroy() {
+		// TODO Auto-generated method stub
+			try {
+			    jpa.destroy("cargo", "cod_cargo", this.getCodigoCargo());
+			    return true;
+			} catch (SQLException e) {
+			    // TODO Auto-generated catch block
+			    return false;
+			}
+
+	}
+	}
---
+++ src/educar/models/AdminModels/CargosNotFound.java
@@ -0,0 +1,10 @@
+package educar.models.AdminModels;
+
+public class CargosNotFound extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9002021480377745651L;
+
+}
