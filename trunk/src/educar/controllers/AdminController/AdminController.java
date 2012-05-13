86b6741558c020cbb9d3d98ddc12e3d299741209
edu.depetris.00@gmail.com <edu.depetris.00@gmail.com@80bb64d5-4173-62ce-5e7d-32d8d86d7bbf>
2012-05-13 01:10:03 +0000

    botones enlazados

--- trunk/src/educar/controllers/AdminController/AdminController.java
+++ trunk/src/educar/controllers/AdminController/AdminController.java
@@ -1,26 +1,125 @@
 package educar.controllers.AdminController;
 
+import java.sql.SQLException;
+import java.util.LinkedList;
+
 import educar.controllers.IController;
 import educar.gui.IView;
 import educar.gui.AdminViews.administrador;
 import educar.languaje.defaultLanguaje;
+import educar.models.AdminModels.Alumno;
+import educar.models.AdminModels.Cargos;
+import educar.models.AdminModels.Docente;
+import educar.models.AdminModels.Facultad;
+import educar.models.AdminModels.Subject;
 
 public class AdminController implements IController, defaultLanguaje {
     private administrador view;
+    private gestionAlumnosController alumnos;
+    private static LinkedList<String> studentsList;
+    private static LinkedList<String> teachersList;
+    private LinkedList<String> subjects;
+    private LinkedList<String> facultades;
+    private static LinkedList<String> facList;
+    private static LinkedList<String> cargosList;
 
     @Override
     public void process(String model) {
+
 	if (model.compareTo(CLOSE_WINDOW) == 0) {
 	    view.dispose();
-	} else {
-	    new administrador();
+	}
+	if (model.compareTo(ALUMNOSPANEL) == 0) {
+	    view.menuAlumnos();
+	    listaAlumnos();
+	}
+	if (model.compareTo(DOCENTESPANEL) == 0) {
+	    view.menuDocentes();
+	    listaDocentes();
+	}
+	if (model.compareTo(MATERIAPANEL) == 0) {
+	    view.menuMateria();
+	    listaMaterias();
+
+	}
+	if (model.compareTo(FACULTADPANEL) == 0) {
+	    view.menuFacultad();
+	    listaFacultad();
+
+	}
+	if (model.compareTo(CARGOSPANEL) == 0) {
+	    view.menuCargos();
+	    listaCargos();
+	}
+
+	if (model.compareTo(DEDICACIONDOCENTEPANEL) == 0) {
+	    view.menuDedicacionDocente();
+	    listaDocentes();
+	    listaFacultad();
+	    listaCargos();
+	}
+	if (model.compareTo(ASIGNACIONDOCENTEPANEL) == 0) {
+	    view.menuAsignacionDocente();
+	    listaDocentes();
+	    listaMaterias();
 	}
 
     }
 
     @Override
     public void setView(IView view) {
-	// TODO Auto-generated method stub
 	this.view = (administrador) view;
     }
+
+    private void listaAlumnos() {
+	try {
+	    studentsList = Alumno.ListAlumnos();
+	    view.setListAlumnos_ABM(studentsList);
+	} catch (SQLException e) {
+	    view.present("no entro por la lista de alumnos");
+	}
+
+    }
+
+    private void listaDocentes() {
+	try {
+	    teachersList = Docente.ListDocente();
+	    view.setListDocente_ABM(teachersList);
+	} catch (SQLException e) {
+	    view.present("no entro por la lista de alumnos");
+	}
+
+    }
+
+    private void listaMaterias() {
+	try {
+	    subjects = Subject.subjectList();
+	    view.setListMateria_ABM(subjects);
+	    facultades = Facultad.ListFacultad();
+	    view.setListFacultad_ABM(facultades);
+	} catch (SQLException e) {
+	    view.present("no entro por la lista de materias");
+	}
+    }
+
+    private void listaFacultad() {
+	try {
+	    facList = Facultad.ListFacultad();
+	    view.setListFacultad_ABM(facList);
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	    view.present("error al actualizar la lista de facultades");
+	}
+    }
+
+    private void listaCargos() {
+	try {
+	    cargosList = Cargos.listaCargos();
+	    view.setListModificarCargos(cargosList);
+	} catch (SQLException e) {
+	    view.present("no entro por la lista de los cargos");
+	}
+
+    }
+
 }
