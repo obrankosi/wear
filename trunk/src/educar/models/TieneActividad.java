4fa6f9185a878728c08955d2e85f87bf8ae5f918
edu.depetris.00@gmail.com <edu.depetris.00@gmail.com@80bb64d5-4173-62ce-5e7d-32d8d86d7bbf>
2012-05-15 23:49:29 +0000

    echo el modelo TieneActividad

--- trunk/src/educar/models/TieneActividad.java
+++ trunk/src/educar/models/TieneActividad.java
@@ -1,10 +1,83 @@
 package educar.models;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
 import educar.db.JPA;
 
 public class TieneActividad {
 
-    private JPA jpa;
-    
-    
+    private static ResultSet rst;
+    private String dniAlumno;
+    private String codActividad;
+
+    /**
+     * @param dniAlumno
+     * @param codAcitividad
+     */
+    public TieneActividad(String dniAlumno, String codAcitividad) {
+	this.dniAlumno = dniAlumno;
+	this.codActividad = codAcitividad;
+
+    }
+
+    /**
+     * @return true ssi el guardado fue exitoso
+     */
+    public boolean save() {
+	String[] columns = { "dni_alumno", "codigo_a" };
+	JPA jpa = new JPA();
+	PreparedStatement stm = jpa.newRecord("educar_dev." + "TieneActividad",
+		columns);
+	try {
+	    stm.setString(1, this.getDniAlumno().trim());
+	    stm.setString(2, this.getCodActividad().trim());
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	    return false;
+	}
+	jpa.create(stm);
+	return true;
+
+    }
+
+    /**
+     * @param dniAlumno
+     * @return LinkedList<String> numero de actividades
+     *  
+     */
+    public static LinkedList<String> listaActividadesAlumnos(String dniAlumno) {
+	LinkedList<String> result = new LinkedList<String>();
+	JPA jpa = new JPA();
+	rst = jpa.proyeccion("TieneActividad","codigo_a", "dni_alumno", dniAlumno);
+	try {
+	    while (rst.next()) {
+	        String codA = rst.getString(1);
+	        result.add((codA).trim());
+	    }
+	} catch (SQLException e) {
+	    System.out.println("erro en la consulta en la base de datos");
+	    return null;
+	}
+	return result;
+    }
+
+    public String getDniAlumno() {
+	return dniAlumno;
+    }
+
+    public void setDniAlumno(String dniAlumno) {
+	this.dniAlumno = dniAlumno;
+    }
+
+    public String getCodActividad() {
+	return codActividad;
+    }
+
+    public void setCodActividad(String codActividad) {
+	this.codActividad = codActividad;
+    }
+
 }
