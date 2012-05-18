eee62c4cfd3d9206a699184f6490b4e9194f4812
coriasaul@gmail.com <coriasaul@gmail.com@80bb64d5-4173-62ce-5e7d-32d8d86d7bbf>
2012-05-18 02:48:10 +0000

    test Alumno, TEst MAteria

---
+++ trunk/test/AlumnoTest.java
@@ -0,0 +1,45 @@
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import educar.models.AdminModels.Alumno;
+
+
+/**	GRUPO WEAR
+ * @author CHICHO
+ *test pasa la alta y baja de un alumno
+ */
+public class AlumnoTest {
+
+	private Alumno alumno;
+
+
+	/**creo un alumno y lo guardo 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	
+		     alumno = new Alumno("32328847","saul", "coria","1998-12-12", "4621722", "blas parera 1742");
+		     alumno.save();
+	}	
+	
+	/**pruebo si puedo insertar de nuevo el mismo alumno ya guardado
+	 * se espera que retorne FALSE sino puede guardarse
+	 */
+	@Test
+	public void guardarElMismoALumno() {
+		 Assert.assertEquals(false, alumno.save());
+	}
+
+	/**pruebo si el alumno creado y guardado anteriormente ahora puedo boorrarlo
+	 * 
+	 */
+	@After
+	public void tearDown(){
+		alumno.destroy();
+	}
+
+}
